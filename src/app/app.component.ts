d66a07f67f7689756f004dd5db48e5faf59753b5
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-16 23:02:56 +0500

    added custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,21 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, FormsModule],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    FormsModule,
+    NgIf,
+    AuthDirective,
+  ],
 })
 export class AppComponent {
   private authService = inject(AuthService);
